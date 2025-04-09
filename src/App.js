63fb378293d131791cadf22f678069d61bd35d8f
DuMing <dmitrii.perfilev2020@gmail.com>
2025-04-09 10:54:50 +0800

    com_123

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./app/store";
-import Home from "./pages/Home";
-import "./styles/Home.css";
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* Здесь будут ваши маршруты */}
         </Routes>
       </Router>
     </Provider>
